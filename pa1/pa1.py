ae908f71ea4334322c40c8e55cb77314f014e4c5
OwenTishenkel-1 <54337648+OwenTishenkel-1@users.noreply.github.com>
2023-02-13 23:38:06 -0500

    most simple methods done

--- pa1/pa1.py
+++ pa1/pa1.py
@@ -1,1 +1,20 @@
-
+import numpy as np;
+import math;
+import random as rand;
+def sigmoid(v):
+    return 1/(1+math.exp(v));
+def perceptron(w,x):
+    return np.matmul(w,x);
+def inputPatternGen():
+    pattern = []
+    for l in range(0,2):
+        for m in range(0,2):
+            for n in range(0,2):
+                for o in range(0,2):
+                    pattern.append(np.array([l,m,n,o]));
+    pattern = np.array(pattern);
+    return pattern;
+def randWeightBias():
+    return np.array([rand.uniform(-1,1),rand.uniform(-1,1),rand.uniform(-1,1),rand.uniform(-1,1),rand.uniform(-1,1)]);
+def desiredOutput(inputPattern):
+    return sum(inputPattern)%2
